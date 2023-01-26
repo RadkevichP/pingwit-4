e2503d0ce1efcd523efc68b19ccfb36e69a079b3
Pavel Radkevich <pradkevich@eisgroup.com>
2023-01-26 20:17:41 +0200

    Second commit

---
+++ src/main/java/pl/pingwit/lec_2/point_2/EmptyClass.java
@@ -0,0 +1,8 @@
+package pl.pingwit.lec_2.point_2;
+
+/**
+ * @author Pavel Radkevich
+ * @since 26.01.23
+ */
+public class EmptyClass {
+}
---
+++ src/main/java/pl/pingwit/lec_2/point_2/SecondEmptyClass.java
@@ -0,0 +1,6 @@
+package pl.pingwit.lec_2.point_2;/**
+*
+ * @author Pavel Radkevich
+ * @since 26.01.23 
+ */public class SecondEmptyClass {
+}
